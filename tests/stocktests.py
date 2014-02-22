782cdc6bf3d722bb6c3d42c3da9c77a1fdc1e3d6
Mike Combs <mike@75design.com>
2014-02-22 15:08:24 -0500

    Improved test to ensure 2 data points loaded

--- tests/stocktests.py
+++ tests/stocktests.py
@@ -6,17 +6,19 @@ class test_stock(unittest.TestCase):
     '''Tests for the stock object.
 
     To run these tests, cd to the PARENT of chelmbigstock, then:
-    python -m chelmbigstock.tests.read_file
+    python -m chelmbigstock.tests.stocktests
 
     '''
 
     def test_load_data(self):
-        '''Make sure at least one data point is loaded'''
+        '''Make sure at least two data points are loaded'''
 
         this_stock = Stock('ibm', 'chelmbigstock/data')
         this_stock.populate()
         self.assertIsNotNone(this_stock.dates[0])
         self.assertIsNotNone(this_stock.values[0])
+        self.assertIsNotNone(this_stock.dates[1])
+        self.assertIsNotNone(this_stock.values[1])
 
 if __name__ == '__main__':
     unittest.main()
