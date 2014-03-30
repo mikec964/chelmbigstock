5d6ced6be7e01a5c1029f7f5a6cf7a89ce52a818
Andy Webber <andygwebber@yahoo.com>
2014-03-30 17:39:19 -0400

    change to deal with overfitting

--- chelmbigstock/chelmbigstock.py
+++ chelmbigstock/chelmbigstock.py
@@ -29,14 +29,38 @@ def main():
     
 
     stocks = []
+    stocks_train = []
+    stocks_cv = []
+    count = 0
     for stock in stock_names:
-        print(stock)
         this_stock = Stock(stock, '../data')
         this_stock.populate()
         stocks.append(this_stock)
+        if count % 2 == 0:
+            stocks_train.append(this_stock)
+        else:
+            stocks_cv.append(this_stock)
+        count = count + 1
 
     training_data = LearningData()
+    cv_data = LearningData()
+    
+    day_history = []
+    for i in range(5, 101, 5):
+        day_history.append(i)
+        
     reference_date = dateutil.days_since_1900('1980-01-01')
+    training_data.construct(stocks_train,[reference_date, day_history, 25])
+    cv_data.construct(stocks_cv,[reference_date, day_history, 25])
+    
+    reference_date = dateutil.days_since_1900('1981-01-01')
+    training_data.append(stocks_train,[reference_date, day_history, 25])
+    cv_data.append(stocks_cv,[reference_date, day_history, 25])
+    
+    """reference_date = dateutil.days_since_1900('1981-01-01')
+    training_data.append(stocks_train,[reference_date, [50, 100, 150], 50])
+    cv_data.append(stocks_cv,[reference_date, [50, 100, 150], 50])"""
+    """reference_date = dateutil.days_since_1900('1980-01-01')
     i_day = dateutil.find_ref_date_idx(stocks[0], reference_date)
    # trainingData.construct(stocks,['1/1/1980', [50, 100, 150], 50])
     training_data.construct(stocks,[reference_date, [50, 100, 150], 50])
@@ -47,7 +71,7 @@ def main():
     reference_date = dateutil.days_since_1900('1982-01-01')
     cv_data.construct(stocks,[reference_date, [50, 100, 150], 50])
     reference_date = dateutil.days_since_1900('1983-01-01')
-    cv_data.append(stocks,[reference_date, [50, 100, 150], 50])
+    cv_data.append(stocks,[reference_date, [50, 100, 150], 50])"""
 	
     XX = training_data.X
     clf = linear_model.Ridge(alpha=0.1, fit_intercept=False)
@@ -56,11 +80,13 @@ def main():
 
     # To look for overfitting the code loops through values of alpha plotting distance between
     # the predicted values and actual data and various alphas
-    alph = 0.01
-    #filename = "output/filename.txt"
-    #dir = os.path.dirname(filename)
+    print(training_data.m, cv_data.m)
+    aa = a = np.array((0, 0, 0, 0))
+    bb = a = np.array((1, 1, 1, 1))
+    print(np.linalg.norm(bb - aa))
+    alph = 0.02
     f = open('alpha.txt', 'w')
-    while alph < 0.2:
+    while alph < 0.2:  #0.2
         # First fit the data for each alpha
         clf = linear_model.Ridge (alpha=alph, fit_intercept=False)
         clf.fit(training_data.X, training_data.y)
@@ -71,8 +97,12 @@ def main():
         # The linagl function in np takes a L2 norm of the difference
         diff_data = (1.0/training_data.m) * np.linalg.norm(predict_data - training_data.y)
         diff_cv = (1.0/cv_data.m) * np.linalg.norm(predict_cv - cv_data.y)
+        print("lengths are ", len(predict_data), len(training_data.y), len(predict_cv), len(cv_data.y))
         # Write out the values
         f.write(str(alph) +  " " + str(diff_data) + " " + str(diff_cv) +  "\n")
+        """print(diff_data, diff_cv)
+        print(predict_data - training_data.y)
+        print(predict_cv - cv_data.y)"""
         alph = alph * 1.5 # Increment alph
     f.close()
      
@@ -88,7 +118,7 @@ def main():
     reference_date = dateutil.days_since_1900('1984-01-01')
     i_day = dateutil.find_ref_date_idx(stocks[0], reference_date)
   #  print (i_day, stocks[0].dates[i_day] )
-    f = open('value.txt', 'w')
+    """ f = open('value.txt', 'w')
     
     while i_day > 100:
         investing_data.construct(stocks,[reference_date, [50, 100, 150], 50])  
@@ -103,7 +133,7 @@ def main():
         #print(portfolio_value)
         i_day = i_day - 50
         reference_date = stocks[0].dates[i_day]
-    f.close()
+    f.close() """
     
     print("run finished")
 
