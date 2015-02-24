8a537de609d54f10e73e4afeca8d1794e560536a
Hideki Ikeda <hideki.ikeda@gmail.com>
2015-02-24 12:24:31 -0500

    Added sample batch to run mkmropt
    Added a sample batch script to run mkmropt.py on Windows

---
+++ mapreduce/mropt.bat
@@ -0,0 +1,10 @@
+@echo off
+rem This is a sample use of mkmropt.py
+rem Input:
+rem       input\mkmropt.ini
+rem Output:
+rem       input\result.csv
+
+rem For details about mkmropt.py, run python mkmropt.py -h
+
+python .\mkmropt.py sample\mkmropt.ini
